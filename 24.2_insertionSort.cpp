ba8c789dd845cd333e0c65c051e1eb93c3a92a3a
Vivek30092 <golu423806@gmail.com>
2026-05-02 23:41:38 +0530

    Sat, 02 May, 2026, 11:41 PM

--- 24.2_insertionSort.cpp
+++ 24.2_insertionSort.cpp
@@ -2,8 +2,20 @@
 #include<iostream>
 using namespace std;
 
-void insertionSort(int arr[], int n){
-    
+void insertionSort(int arr[], int n){           //O(n^2)
+    for(int i = 1; i<n; i++){               //traverse unsorted array
+        int curr = arr[i];
+        int prev = i-1;
+
+        // while(prev >=0 && arr[prev] > curr){    //for ascending
+        while(prev >=0 && arr[prev] < curr){    // for descending
+
+            arr[prev+1] = arr[prev];
+            prev--;
+        }
+
+        arr[prev+1] = curr; //placing the curr el in its correct position
+    }
 }
 
 void printArray(int arr[], int n){
