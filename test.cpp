cc1ee722a95971fd1384290015c1085e84521c46
Vivek30092 <golu423806@gmail.com>
2026-05-05 23:44:03 +0530

    Tues, 5 May, 2026, 11:43 PM

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+int main(){
+    vector<int> nums = {9,6,4,2,3,5,7,0,1};
+    int size = nums.size()
+
+    int min= *min_element;
+    
+
+    return 0;
+}
