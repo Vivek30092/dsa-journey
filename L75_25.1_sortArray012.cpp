18353630cfd69822fede72513e3eddd340a864f1
Vivek30092 <golu423806@gmail.com>
2026-06-03 19:31:22 +0530

    bug fixes

--- L75_25.1_sortArray012.cpp
+++ L75_25.1_sortArray012.cpp
@@ -1,32 +1,44 @@
 // Sort array with 0s, 1s & 2s
 // optimized approach
+// TC : O(n)
 #include<iostream>
 #include<vector>
 using namespace std;
 
-void sortColor(vector<int> arr, int num){
-    int count0,count1,count2 = 0;
+void sortColor(vector<int> &nums, int num){
+    int count0 = 0,count1 = 0,count2 = 0;
+    // O(n)
     for(int i = 0; i<num; i++){
-        if(arr[i] == 0) count0++;
-        else if(arr[i] == 1) count1++;
-        else if(arr[i] == 2) count2++;
-        else{
-            cout<<"invalid input";
-        }
+        if(nums[i] == 0) count0++;
+        else if(nums[i] == 1) count1++;
+        else count2++;
+    }
+
+    // O(n)
+    int idx = 0;
+    for(int i = 0; i<count0;i++){
+        nums[idx++] = 0;
+    }
+
+    for(int i = 0; i<count1;i++){
+        nums[idx++] = 1;
+    }
+    for(int i = 0; i<count2;i++){
+        nums[idx++] = 2;
     }
     return;
 }
 
-void printValues(vector<int> arr, int size){
-    for(int val : arr){
+void printValues(vector<int> &nums, int size){
+    for(int val : nums){
         cout<<val<< " ";
     }
     return;
 }
 int main(){
-    vector<int> arr = {2,0,2,1,1,0,1,2,0,0};
-    int size = arr.size();
-    sortColor(arr,size);
-    printValues(arr,size);
+    vector<int> nums = {2,0,2,1,1,0,1,2,0,0};
+    int size = nums.size();
+    sortColor(nums,size);
+    printValues(nums,size);
     return 0;
 }
