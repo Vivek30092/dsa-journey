a4a456a0148d66544087da728468640fda6c329c
Vivek30092 <golu423806@gmail.com>
2026-06-06 00:25:13 +0530

    Sat, 06 June, 2026, 12:25 AM

--- L75_25.2_sortArray012.cpp
+++ L75_25.2_sortArray012.cpp
@@ -2,4 +2,49 @@
 // most optimized approach with new concept
 // TC : O(n) solution will be found in single pass
 // Using Dutch National Flag algorithm
+/*          
+            Logic used: 
+Three pointer : low, mid, high
+1.low: 0 to l-1
+2.mid: l to m-1
+complete unsorted part reside in mid to high between mid and high
+[move mid:forward | high: backwork, until mid==high(unsorted part becomes zero(0).)]
+3.high: h+1 to n-1  (n total element)
 
+in starting mid, low = 0 and high = n-1
+*/
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+void sortColor(vector<int> &nums,int size){
+    int low = 0, mid = 0, high = size-1;
+    while (mid<=high)
+    {
+        if(nums[mid]==0){
+            swap(nums[low],nums[mid]);
+            mid++; low++;
+        }else if (nums[mid] == 1){
+            mid++;
+        }else{
+            swap(nums[high],nums[mid]);
+            high--;
+        }
+    }
+    
+
+}
+
+void printValues(vector<int> &nums, int size){
+    for(int val:nums){
+        cout<<val<<" ";
+    }
+}
+int main(){
+    vector<int> nums = {2,0,2,1,1,0,1,2,0,0};
+    int size = nums.size();
+    sortColor(nums,size);
+    printValues(nums,size);
+    return 0;
+}
