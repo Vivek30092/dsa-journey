9dabf4ff892f156598d3dbb7c32d1c4b903c27f0
Vivek30092 <golu423806@gmail.com>
2026-06-14 23:52:49 +0530

    Sun, 14 June, 2026, 11:52

---
+++ 27.0_STL.cpp
@@ -0,0 +1,13 @@
+// Standard Template Library 
+/*
+1. Containers
+2. Algorithms
+3. Iterators
+4. Functors
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    // Vectors(containers)
+}
