8e292c7459b6ef213a9bebb7e3af29c798104b8a
Vivek30092 <golu423806@gmail.com>
2026-06-29 23:39:16 +0530

    Mon, 29 June, 2026, 11:39 PM

--- 27.9_algorithm.cpp
+++ 27.9_algorithm.cpp
@@ -5,9 +5,20 @@ int main(){
     // sorting algorithm
     int arr[5] = {30,14,99, 24, 5};
     sort(arr,arr+5);
+    cout<<"ASC order: ";
     for(auto val: arr){
         cout<<val<<" ";
     }
     cout<<endl;
     // arr+n : because above function does't include last value so we pass last of (array + 1: out of the array) value index
+
+
+    
+    // to sort in DESC order
+    sort(arr,arr+5,greater<int>());          //third parameter is a functor/comparator(function which tell the logic of comparison)
+    cout<<"DESC order: ";
+    for(auto val: arr){
+        cout<<val<<" ";
+    }
+    cout<<endl;
 }
